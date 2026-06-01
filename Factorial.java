d865c7abf2c0168016ca32c30585378c410b793f
Fritz-2008 <fritzanimaciones@gmail.com>
2026-06-01 09:32:00 -0500

    Implement factorial calculation in Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese un número: ");
+        int n = sc.nextInt();
+
+        long fact = 1;
+        int i = 1;
+
+        while (i <= n) {
+            fact *= i;
+            i++;
+        }
+
+        System.out.println("Factorial: " + fact);
+    }
+}
